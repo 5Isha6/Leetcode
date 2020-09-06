3f1eac726221ff18b4a12723eb3b7fc96aae0b8f
isha.sh <isha.sh>
2020-09-06 17:55:32 +0530

    June 11

---
+++ 78. Subsets.py
@@ -0,0 +1,10 @@
+# 78. Subsets
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = [[]]
+        for i in (nums):
+            output += [lst+[i] for lst in output]
+        
+        
+        
+        return output
