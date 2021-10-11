d6d84833332a0763dd0ae34fc0bcf416b903d294
5Isha6 <62375008+5Isha6@users.noreply.github.com>
2021-10-11 11:07:14 -0400

    Create 1200. Minimum Absolute Difference.py

---
+++ 1200. Minimum Absolute Difference.py
@@ -0,0 +1,7 @@
+# 1200. Minimum Absolute Difference
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        mini = min(b-a for a,b in zip(arr,arr[1:]))
+        return [[a,b] for a,b in zip(arr,arr[1:]) if b - a  == mini]
+        
